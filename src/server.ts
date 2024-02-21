a024281e7959e468a4910053279810302ca50ca9
Abhinav Nallapareddy <abhinav@Abhinavs-MacBook-Pro-4.local>
2024-02-21 13:07:13 -0500

    Code runs on Mac

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,41 @@ import { SERVER_PORT} from "./constants";
 const cors = require('cors');
 const app = express();
 
+function findPath(inputString:string) {
+  // Find the index of the last occurrence of '/'
+  const arr = inputString.split('/');
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] == "take-home-assignment-electron") {
+      console.log(arr.slice(0,i+1).join("/"));
+      return isWin() ? arr.slice(0,i+1).join("/")+"/src/python/main.exe" : arr.slice(0,i+1).join("/")+"/src/python/dist/main";
+    }
+  }
+
+  throw new Error("Path Not Recognized. Make sure Folder isn't renamed");
+}
+
+//Check what Operating System currently is
+function isWin() {
+  if (process.platform === 'win32') {
+    console.log('Current system is Windows');
+    return true;
+  } else if (process.platform === 'darwin') {
+    console.log('Current system is macOS');
+    return false;
+  } else {
+    throw new Error("Current system is not Windows or macOS");
+  }
+}
+
 // Define a route for the GET request
   app.get('/', (req: Request, res: Response) => {
     const { spawn } = require('child_process');
-    //const pythonProcess = spawn('python3', ['./python/main.py','greet','Daniel']);
-    const pythonProcess = spawn('python3', ['./python/main.py','square','4']);
+    const pathName = findPath(__dirname);
+    //const pythonProcess = spawn(pathName, ['square','4']);
+    const pythonProcess = spawn(pathName, ['greet','Daniel']);
 
-// Listen for data from the Python script
+    // Listen for data from the Python script
     pythonProcess.stdout.on('data', (data: any) => {
       const tJSON = {
         result: data.toString(),
@@ -37,4 +65,6 @@ const app = express();
   app.listen(SERVER_PORT, () => {
     console.log(`Server is running on http://localhost:${SERVER_PORT}`);
   });
+
+//module.exports = app;
  
